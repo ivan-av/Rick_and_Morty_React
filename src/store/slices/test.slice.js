455588145d574f77ca5e2ada2de1eb5090d26421
Iván Avila <ivan.avicamp@gmail.com>
2022-09-22 03:11:45 -0500

    First commit

---
+++ src/store/slices/test.slice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+// Cambiamos mySlice por el nombre de nuestro slice (usersSlice, toDosSlice...)
+export const mySlice = createSlice({
+  name: '',
+  initialState: undefined,
+  reducers: {
+
+  }
+})
+
+export const { } = mySlice.actions
+
+export default mySlice.reducer
